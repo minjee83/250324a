1f5ea54c228bb37d24f817901b1938a9c5f80718
구민지 <mjg9530@gmail.com>
2025-04-09 09:44:59 +0900

    [UPDATE}test code

--- step07_cicd/src/main/java/edu/fisa/ce/controller/Controller.java
+++ step07_cicd/src/main/java/edu/fisa/ce/controller/Controller.java
@@ -3,11 +3,19 @@ package edu.fisa.ce.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class Controller {
-@GetMapping("fisa1")
-public String reqRes() {
-	System.out.println("req() *******");
-	return "응답요청 성공";
-}
+	@GetMapping("fisa1")
+	public String reqRes() {
+		System.out.println("req() *******");
+		log.info("****요청 & 응답****");
+		for(int i=1; i<=10; i++) {
+			System.out.println("data " + i);
+		}
+		return "응답요청 성공";
+	}
 }
--- step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
+++ step07_cicd/src/test/java/edu/fisa/ce/Step07CicdApplicationTests.java
@@ -1,13 +1,21 @@
 package edu.fisa.ce;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Step07CicdApplicationTests {
-
+	
 	@Test
-	void contextLoads() {
+	public void testAddition() {
+		int result = 2 + 3;
+		
+		
+		System.out.println("비교 전*******************");
+		assertEquals(5, result);
+		
+		System.out.println("비교 후*******************");
 	}
-
 }
